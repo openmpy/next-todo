0135185d1cf747397fc24d98318f7bbf12640f08
openmpy <suhwan@kakao.com>
2024-06-19 14:18:48 +0900

    Profile actions & permanentRedirect

--- app/share/[user_id]/page.tsx
+++ app/share/[user_id]/page.tsx
@@ -1,3 +1,6 @@
+import { getProfileById } from "@/actions/auth/user.action";
+import { permanentRedirect } from "next/navigation";
+
 interface SharePageProps {
   params: { user_id: string };
   searchParams: {};
@@ -5,8 +8,12 @@ interface SharePageProps {
 
 const page = async (props: SharePageProps) => {
   const userId = props?.params?.user_id;
+  const profile = await getProfileById({ serverComponent: true, userId });
 
-  console.log(">> userId", userId);
+  console.log(">> profile", profile);
+  if (!profile) {
+    permanentRedirect("/");
+  }
 
   return <div>Share Page</div>;
 };
